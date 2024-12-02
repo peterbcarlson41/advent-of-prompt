24f83932b905f79b9b156cd8ef577d942d311412
Peter Carlson <peterbcarlson41@gmail.com>
2024-12-01 21:37:40 -0500

    rename subfolders

--- day-01/src/components/ThreeCountdown.tsx
+++ day-01/src/components/ThreeCountdown.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 interface ThreeCountdownProps {}
 
